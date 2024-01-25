a4322b7c08102d2fc7de4502cf14713f0b89a215
Talya-Perets <155684002+Talya-Perets@users.noreply.github.com>
2024-01-25 14:59:16 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include "NumClass.h"
+int printAllPrimes(int num1 , int num2){
+        printf("The Prime numbers are:");
+    while(num2>=num1){
+        if(isPrime(num1)){
+            printf("%d ",num1);
+        }
+        num1++;
+
+    }
+
+printf("\n");
+return 0;
+}
+int printAllArmstrong(int num1,int num2){
+        printf("The Armstrong numbers are: ");
+     while(num2>=num1){
+        if(isArmstrong(num1)){
+            printf("%d ",num1);
+        }
+        num1++;
+
+    }
+
+printf("\n");
+return 0;
+}
+
+int printAllStrongNums(int num1,int num2){
+        printf("The Strong numbers are: ");
+    while(num2>=num1){
+        if(isStrong(num1)){
+            printf("%d ",num1);
+        }
+        num1++;
+
+    }
+
+printf("\n");
+return 0;
+}
+
+int PrintAllpalindrome(int num1,int num2){
+        printf("The Palindromes are :");
+       while(num2>=num1){
+        if(isPalindrome(num1)){
+            printf("%d ",num1);
+        }
+        num1++;
+
+    }
+
+printf("\n");
+return 0;
+}
+int main(){
+    int num1,num2;
+    scanf("%d %d", &num1, &num2);
+
+    int smallerNum,bigerNum;
+    if (num1>num2)
+    {
+        smallerNum=num2;
+        bigerNum=num1;
+    }else{
+        smallerNum=num1;
+        bigerNum=num2;
+    }
+    printAllArmstrong(smallerNum,bigerNum);
+    PrintAllpalindrome(smallerNum,bigerNum);
+    printAllPrimes(smallerNum,bigerNum);
+    printAllStrongNums(smallerNum,bigerNum);
+
+
+
+    
+    return 0;
+}
