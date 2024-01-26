9a44947a14b0f7eaac436f860e5d069a7e44f531
Talya-Perets <155684002+Talya-Perets@users.noreply.github.com>
2024-01-26 10:11:52 +0200

    Update advancedClassificationRecursion.c

--- advancedClassificationRecursion.c
+++ advancedClassificationRecursion.c
@@ -1,61 +1,45 @@
 #include "NumClass.h"
+  int countDig(int num){
+    int count =0;
+    while(num>0){
+        num=num/10;
+        count++;
+    }
+    return count;
 
-int power(int base, int exponent) {
-    int result = 1;
-    while (exponent > 0) {
-        result *= base;
+  }
+  int power(int bace ,int exponent){
+    int result =1;
+    while(exponent>0){
+        result*=bace;
         exponent--;
     }
     return result;
-}
+  }
+ 
+    int isArmstrong(int num){
+        if (num<0){return 0;}
+        if (num==0){return 1;}
+        int count =countDig(num);
+        int sum=0;
+        int num1=num;
+        while (num >0){
+            int digit= num%10;
+            sum+=power(digit,count);
+            num/=10;
+            
+        }
 
-int countDig(int num) {
-    if (num == 0) return 0;
-    return 1 + countDig(num / 10);
-}
-
-int isArmstrongHelper(int num, int numDigits, int originalNum) {
-    if (num == 0) {
-        return (originalNum == 0);
-    } else {
-        int digit = num % 10;
-        return (power(digit, numDigits) + isArmstrongHelper(num / 10, numDigits, originalNum));
+         return (sum==num1);
     }
-}
-
-int isArmstrong(int num) {
-    int numDigits = countDig(num);
-    return (isArmstrongHelper(num, numDigits, num) == num);
-}
-
-
-int isPalindromeHelper(int num, int original, int digits) {
-    // Base case: if there is only one digit, it's a palindrome
-    if (digits <= 1) {
-        return 1;
+   
+    int isPalindrome(int num){
+        int reverse =0;
+        int num1 = num;
+        while(num>0){
+            int digit =num%10;
+            reverse=(reverse*10)+digit;
+            num=num/10;
+        }
+       return (num1==reverse);
     }
-
-    // Extract the first and last digits
-    int firstDigit = num / (power(10, digits - 1));
-    int lastDigit = num % 10;
-
-    // Check if the first and last digits are the same
-    if (firstDigit != lastDigit) {
-        return 0;
-    }
-
-    // Remove the first and last digits
-    int newNum = (num % (power(10, digits - 1)) / 10);
-
-    // Recursive call with the reduced number
-    return isPalindromeHelper(newNum, original, digits - 2);
-}
-
-int isPalindrome(int num) {
-    if (num < 0) {
-        return 0; // Negative numbers are not palindromes
-    }
-
-    int digits = countDig(num);
-    return isPalindromeHelper(num, num, digits);
-}
